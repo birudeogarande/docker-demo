29466996da3a415d743b4eaa0e36530ebf5efeef
Birudeo Garande <birudeo_garande@bmc.com>
2024-03-17 23:53:14 +0530

    added simple api  to test

--- src/main/java/com/saatvik/MyAppApplication.java
+++ src/main/java/com/saatvik/MyAppApplication.java
@@ -1,13 +1,26 @@
 package com.saatvik;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class MyAppApplication {
 
+	private Logger logger = LoggerFactory.getLogger(getClass());
+	
 	public static void main(String[] args) {
 		SpringApplication.run(MyAppApplication.class, args);
+		
 	}
 
+	@GetMapping("/hello")
+	public String gretting() {
+		logger.info("Calling hello API");
+		return "Hello, Welcome to docker demo";
+	}
 }
